093fdbfa80c865cf6cb60f48c820d89e1191a2ed
hare32 <234694@edu.p.lodz.pl>
2023-10-31 11:18:34 +0100

    Podzial na kategorie i logo w main

--- Lumplet/app/src/main/java/com/example/lumplet/ItemList.java
+++ Lumplet/app/src/main/java/com/example/lumplet/ItemList.java
@@ -9,33 +9,32 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.ListenerRegistration;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
-
+import java.util.HashMap;
+import java.util.Map;
 
 public class ItemList extends AppCompatActivity {
 
     private FirebaseFirestore db;
-    private DocumentReference snkrsDocRef;
     private ListView listView;
+    TextView kategoriaTextView;
+    private final ArrayList<Item> snkrsList = new ArrayList<>();
+    private final ArrayList<Item> clothesList = new ArrayList<>();
+    private final ArrayList<Item> accessoriesList = new ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.item_list);
-        TextView kategoriaTextView = findViewById(R.id.kategoriaTextView);
+        kategoriaTextView = findViewById(R.id.kategoriaTextView);
         db = FirebaseFirestore.getInstance();
-        snkrsDocRef = db.collection("items").document("snkrs1");
         listView = findViewById(R.id.listView);
 
         Bundle extras = getIntent().getExtras();
@@ -46,41 +45,52 @@ public class ItemList extends AppCompatActivity {
             }
         }
     }
+
     @Override
     protected void onStart() {
         super.onStart();
 
-        // Obsługa błędów
-        // Aktualizuj listę z danymi produktów
-        // Przekaz listę do adaptera i zaktualizuj ListView
-        ListenerRegistration snkrsListener = snkrsDocRef.addSnapshotListener(new EventListener<DocumentSnapshot>() {
-            @Override
-            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException error) {
-                if (error != null) {
-                    // Obsługa błędów
-                    return;
-                }
+        db.collection("items")
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            String name = document.getString("name");
+                            double price = document.getDouble("price");
+                            String category = document.getString("category");
+                            String description = document.getString("description");
 
-                if (documentSnapshot.exists()) {
-                    String name = documentSnapshot.getString("name");
-                    double price = documentSnapshot.getDouble("price");
-                    String category = documentSnapshot.getString("category");
-                    String description = documentSnapshot.getString("description");
+                            Item item = new Item(name, price, category, description);
+                            if (category != null) {
+                                switch (category) {
+                                    case "sneakers":
+                                        snkrsList.add(item);
+                                        break;
+                                    case "cloth":
+                                        clothesList.add(item);
+                                        break;
+                                    case "accessory":
+                                        accessoriesList.add(item);
+                                        break;
+                                }
+                            }
+                        }
 
-                    // Aktualizuj listę z danymi produktów
-                    Item item = new Item(name, price, category, description);
-                    ArrayList<Item> itemList = new ArrayList<>();
-                    itemList.add(item);
-
-                    // Przekaz listę do adaptera i zaktualizuj ListView
-                    ProductAdapter adapter = new ProductAdapter(ItemList.this, itemList);
-                    listView.setAdapter(adapter);
-                }
-            }
-        });
+                        // Przekaz odpowiednią listę do adaptera i zaktualizuj ListView
+                        String currentCategory = kategoriaTextView.getText().toString();
+                        if (currentCategory.equalsIgnoreCase("Obuwie")) {
+                            ProductAdapter adapter = new ProductAdapter(ItemList.this, snkrsList);
+                            listView.setAdapter(adapter);
+                        } else if (currentCategory.equalsIgnoreCase("Ubrania")) {
+                            ProductAdapter adapter = new ProductAdapter(ItemList.this, clothesList);
+                            listView.setAdapter(adapter);
+                        } else if (currentCategory.equalsIgnoreCase("Akcesoria")) {
+                            ProductAdapter adapter = new ProductAdapter(ItemList.this, accessoriesList);
+                            listView.setAdapter(adapter);
+                        }
+                    } else {
+                        throw new IllegalArgumentException("Błąd w przekazaniu listy");
+                    }
+                });
     }
-
 }
-
-
-
